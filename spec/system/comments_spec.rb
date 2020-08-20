b9252829d290c4b5b23f088e00463c7001d6cda9
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-08-20 19:28:52 +0900

    コメントテスト結合コード

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'コメント投稿', type: :system do
   before do
-    @tweet = FactoryBot.create(:tweets)
-    @comment = Faker::Lolem.sentence
+    @tweet = FactoryBot.create(:tweet)
+    @comment = Faker::Lorem.sentence
   end
 
   it 'ログインしたユーザーはツイート詳細ページでコメント投稿できる' do
     # ログインする
     visit new_user_session_path
-    fill_in 'Email' , wirh: @tweet.user.email
-    fill_in 'Password', with: @tweett.user.password
+    fill_in 'Email' , with: @tweet.user.email
+    fill_in 'Password', with: @tweet.user.password
     find('input[name="commit"]').click
     expect(current_path).to eq root_path
     # ツイート詳細ページに遷移する
@@ -20,11 +20,11 @@ RSpec.describe 'コメント投稿', type: :system do
     # コメントを送信すると、Commentsモデルのカウントが1上がる事を確認する
     expect{
       find('input[name="commit"]').click
-    }.to change { Commnt.count} .by(1)
+    }.to change { Comment.count} .by(1)
     # 詳細ページにリダイレクトされる事を確認する
     expect(current_path).to eq tweet_path(@tweet)
     # 詳細ページ上に先程のコメント内容が含まれている事を確認する
     expect(page).to have_content @comment
   end
-  
+
 end
