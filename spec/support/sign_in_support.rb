db0d3759df5db323c1fdb46141c6942092ff1a16
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-08-21 12:04:10 +0900

    結合テストコードをまとめよう

---
+++ spec/support/sign_in_support.rb
@@ -0,0 +1,10 @@
+module SignInSupport
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in 'Email' , with: user.Email
+    fill_in 'Password', with: user.Password
+    find('input[name="commit"]') .click
+    expect(current_path).to eq root_path
+  end
+end
+
