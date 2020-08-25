d41e62c8d90936488b4b8cce52fb89421c70b2b3
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-08-26 06:13:07 +0900

    サービスクラスの実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,10 +4,10 @@ class Tweet < ApplicationRecord
   has_many :comments  # commentsテーブルとのアソシエーション
 
   def self.search(search)
-    if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%")
-    else
-      Tweet.all
-    end
+   if search != ""
+    Tweet.where('text LIKE(?)', "%#{search}%")
+   else
+    Tweet.all
+   end
   end
 end
